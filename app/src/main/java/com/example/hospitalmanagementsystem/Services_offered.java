bac3357c9731808b9aaf385e907b90f90f02a5ab
MOHIT BHORIA <bhoria625@gmail.com>
2018-03-30 22:53:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hospitalmanagementsystem/Services_offered.java
@@ -0,0 +1,15 @@
+package com.example.hospitalmanagementsystem;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Neeraj on 14-Apr-16.
+ */
+public class Services_offered extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.services_offered);
+
+    }
+}
