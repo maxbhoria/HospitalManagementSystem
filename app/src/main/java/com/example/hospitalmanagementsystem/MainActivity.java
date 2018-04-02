f74417881832dc8517da4a4b290f69c6be3772a3
MOHIT BHORIA <bhoria625@gmail.com>
2018-04-02 23:19:47 +0530

    splash added

--- app/src/main/java/com/example/hospitalmanagementsystem/MainActivity.java
+++ app/src/main/java/com/example/hospitalmanagementsystem/MainActivity.java
@@ -5,13 +5,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     DatabaseHelper databaseHelper;
     Toolbar toolbar;
-    Button bLogInRegister;
+    LinearLayout patient_login,doctor_login,staff_login,admin_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +23,32 @@ public class MainActivity extends AppCompatActivity {
         //SET UP THE DATABASE
         databaseHelper = new DatabaseHelper(this);
         //SQLiteDatabase sqLiteDatabase = databaseHelper.getWritableDatabase();
-        bLogInRegister = findViewById(R.id.bloginregister);
-        bLogInRegister.setOnClickListener(new View.OnClickListener() {
+        patient_login = findViewById(R.id.patient_login);
+        doctor_login = findViewById(R.id.doctor_login);
+        staff_login = findViewById(R.id.staff_login);
+        admin_login = findViewById(R.id.admin_login);
+        patient_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Login.class);
+                startActivity(i);
+            }
+        });
+        doctor_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Login.class);
+                startActivity(i);
+            }
+        });
+        staff_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Login.class);
+                startActivity(i);
+            }
+        });
+        admin_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this, Login.class);
@@ -34,12 +58,4 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void onClick(View view) {
-        Intent i = new Intent(MainActivity.this, Doctors_available.class);
-        startActivity(i);
-    }
-    public void onClick2(View view) {
-        Intent i = new Intent(MainActivity.this, Services_offered.class);
-        startActivity(i);
-    }
 }
